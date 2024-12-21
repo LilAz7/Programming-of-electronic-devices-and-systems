1301429a327a5f2dbed7a6bd705476e53a895e06
Andrey Cheban <77441445+LilAz7@users.noreply.github.com>
2024-12-21 13:22:09 +0300

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,16 +1,26 @@
-const int startStopButtonPin = 2; 
-const int resetButtonPin = 3;     
+#include <TM1637Display.h>
+
+#define CLK 2
+#define DIO 3
+
+TM1637Display display(CLK, DIO);
+
+const int startStopButtonPin = 4; 
+const int resetButtonPin = 5;     
 
 bool isRunning = false;           
-unsigned long startTime = 0;      
-unsigned long elapsedTime = 0;    
+unsigned long startTime = 0;     
+int timerDuration = 120;         
+int remainingTime = timerDuration; 
 bool lastStartStopState = LOW;    
-bool lastResetState = LOW;        
+bool lastResetState = LOW;       
 
 void setup() {
   pinMode(startStopButtonPin, INPUT_PULLUP); 
-  pinMode(resetButtonPin, INPUT_PULLUP);     
-  Serial.begin(9600);                        
+  pinMode(resetButtonPin, INPUT_PULLUP);    
+
+  display.setBrightness(7);                  
+  display.showNumberDecEx(0b0000, 0, true);  
 }
 
 void loop() {
@@ -20,23 +30,35 @@ void loop() {
   if (startStopState == LOW && lastStartStopState == HIGH) {
     isRunning = !isRunning; 
     if (isRunning) {
-      startTime = millis() - elapsedTime; 
+      startTime = millis(); 
     }
   }
   lastStartStopState = startStopState;
 
   if (resetState == LOW && lastResetState == HIGH) {
-    elapsedTime = 0; 
-    isRunning = false;
+    remainingTime = timerDuration; 
+    isRunning = false;             
+    updateDisplay();               
   }
   lastResetState = resetState;
 
   if (isRunning) {
-    elapsedTime = millis() - startTime;
+    remainingTime = timerDuration - (millis() - startTime) / 1000;
+    if (remainingTime <= 0) {
+      remainingTime = 0;
+      isRunning = false;
+    }
   }
 
-  Serial.print("Секундомер: ");
-  Serial.println(elapsedTime / 1000.0, 2); 
+  updateDisplay();
   delay(100); 
 }
 
+void updateDisplay() {
+  int minutes = remainingTime / 60; 
+  int seconds = remainingTime % 60; 
+
+  int displayTime = (minutes * 100) + seconds;
+
+  display.showNumberDecEx(displayTime, 0b01000000, true); // Двоеточие включено
+}
